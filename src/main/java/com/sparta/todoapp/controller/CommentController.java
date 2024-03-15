d32ea7ee3e6e4892b50ad4d32217e627fe6cc588
oyeonjoo <oyj0842@naver.com>
2024-03-15 18:18:49 +0900

    :art: security 추가에 따른 코드 수정

--- src/main/java/com/sparta/todoapp/controller/CommentController.java
+++ src/main/java/com/sparta/todoapp/controller/CommentController.java
@@ -3,13 +3,19 @@ package com.sparta.todoapp.controller;
 import com.sparta.todoapp.dto.request.CommentRequestDto;
 import com.sparta.todoapp.dto.response.CommentResponseDto;
 import com.sparta.todoapp.global.commonDto.ResponseDto;
-import com.sparta.todoapp.global.jwt.JwtUtil;
 import com.sparta.todoapp.service.CommentService;
 import com.sparta.todoapp.userDetails.UserDetailsImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/todos/{todoId}/comments")
@@ -22,7 +28,7 @@ public class CommentController {
     public ResponseEntity<ResponseDto<CommentResponseDto>> createComment(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
         @PathVariable Long todoId,
-        @RequestBody CommentRequestDto requestDto) {
+        @RequestBody @Valid CommentRequestDto requestDto) {
 
         return ResponseEntity.ok()
             .body(ResponseDto.<CommentResponseDto>builder()
@@ -36,12 +42,13 @@ public class CommentController {
         @AuthenticationPrincipal UserDetailsImpl userDetails,
         @PathVariable Long todoId,
         @PathVariable Long commentId,
-        @RequestBody CommentRequestDto requestDto) {
+        @RequestBody @Valid CommentRequestDto requestDto) {
 
         return ResponseEntity.ok()
             .body(ResponseDto.<CommentResponseDto>builder()
                 .message("수정 성공")
-                .data(commentService.updateComment(userDetails.getUsername(), todoId, commentId, requestDto))
+                .data(commentService.updateComment(userDetails.getUsername(), todoId, commentId,
+                    requestDto))
                 .build());
 
     }
@@ -49,7 +56,6 @@ public class CommentController {
     @DeleteMapping("/{commentId}")
     public ResponseEntity<String> deleteComment(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
-
         @PathVariable Long todoId,
         @PathVariable Long commentId) {
 
